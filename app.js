a12f16c74967edc5730bc8088940f4fe6542178e
ThomassMathisen <thomasmathisen@hotmail.com>
2022-11-03 17:27:21 +0100

    Prepare for deploy

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,10 +9,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression')
-
-const dotenv = require("dotenv")
-
-dotenv.config()
 
 var indexRouter = require('./routes/index');
 
